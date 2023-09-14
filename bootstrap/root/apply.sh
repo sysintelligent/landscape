5abd69cd960b463190e91790b392b28ae2790333
David Gu <sysintelligent@gmail.com>
2023-09-13 19:34:58 -0700

    Restructure the code

---
+++ bootstrap/root/apply.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+VALUES="values.yaml"
+
+# kubectl get ingress gitea --namespace gitea \
+#     || VALUES="values-seed.yaml"
+
+# helm template \
+#     --include-crds \
+#     --namespace argocd \
+#     --values "${VALUES}" \
+#     argocd . \
+#     | kubectl apply -n argocd -f -
+
+helm template \
+    --include-crds \
+    --namespace argocd \
+    --values "${VALUES}" \
+    argocd .
