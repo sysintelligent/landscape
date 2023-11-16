c8a0e90423d0fff7d499589f6e61646064566ca4
David Gu <sysintelligent@gmail.com>
2023-11-15 16:40:38 -0800

    Working Edition

--- bootstrap/argocd/apply.sh
+++ bootstrap/argocd/apply.sh
@@ -13,13 +13,7 @@ helm template \
     argocd . \
     | kubectl apply -n argocd -f -
 
-# helm template \
-#     --dependency-update \
-#     --include-crds \
-#     --namespace argocd \
-#     argocd . \
-#     | kubectl apply -n argocd -f -
-
+# If you want to install argocd with the latest version using the manifests
 # kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
 kubectl -n argocd wait --timeout=60s --for condition=Established \
