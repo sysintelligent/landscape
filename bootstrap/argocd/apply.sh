07d335385523341949aa145cd109b9904a02d79b
David Gu <sysintelligent@gmail.com>
2023-11-15 16:38:33 -0800

    Update Makefile

--- bootstrap/argocd/apply.sh
+++ bootstrap/argocd/apply.sh
@@ -13,6 +13,15 @@ helm template \
     argocd . \
     | kubectl apply -n argocd -f -
 
+# helm template \
+#     --dependency-update \
+#     --include-crds \
+#     --namespace argocd \
+#     argocd . \
+#     | kubectl apply -n argocd -f -
+
+# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
 kubectl -n argocd wait --timeout=60s --for condition=Established \
        crd/applications.argoproj.io \
        crd/applicationsets.argoproj.io
